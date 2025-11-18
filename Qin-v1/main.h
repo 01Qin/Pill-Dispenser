4e5abb98e0a23aa2a92964eda097fe64b67663a3
01Qin <sisukasqin@hotmail.com>
2025-11-18 21:16:00 +0200

    Add several function declarations for motor and sensor setup

--- Qin-v1/main.h
+++ Qin-v1/main.h
@@ -46,4 +46,12 @@ bool light_switch(const uint *leds, uint brightness, bool on);
 void set_brightness(const uint *leds, uint brightness);
 uint clamp_to_wrap(int bright_value);
 
+void ini_coil_pins(const uint *coil_pins); // Initialize motor coil output pins as outputs
+void ini_sensor(); // Initialize optical sensor input with internal pull-up
+int do_calibration(const uint *coil_pins, const uint8_t sequence[8][4], int max, int revolution_steps[3]); // Measure steps per revolution using the optical sensor
+void step_motor(const uint *coil_pins, int step, const uint8_t sequence[8][4]); // Perform one half-step of the stepper motor
+int get_avg(const int revolution_steps[3]); // Calculate the average of three revolution step counts
+void run_motor(const uint *coil_pins, const uint8_t sequence[8][4], int count, int steps_per_rev); // Run the motor for N * (1/8) revolutions using the calibrated steps per revolution
+
+
 #endif //QIN_V1_MAIN_H
